61a0e2bddc93d26b50a78c60200afee0a258138f
j0nathas <jonathasoliveira.2005@gmail.com>
2026-05-13 08:17:19 -0300

    Estrutura de pastas e arquivos

---
+++ backend/src/main/java/com/jonathas/gerenciamento_ferias/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,9 @@
+package com.jonathas.gerenciamento_ferias.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+}
--- backend/src/main/java/com/jonathas/gerenciamento_ferias/controller/HelloWorldController.java
+++ backend/src/main/java/com/jonathas/gerenciamento_ferias/controller/HelloWorldController.java
@@ -1,15 +1,36 @@
 package com.jonathas.gerenciamento_ferias.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jonathas.gerenciamento_ferias.service.HelloWorldService;
+import com.jonathas.gerenciamento_ferias.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello-world")
+
+
 public class HelloWorldController {
+
+    @Autowired
+
+    private HelloWorldService helloWorldService;
+
+    /*
+    public HelloWorldController(HelloWorldService helloWorldService){
+        this.helloWorldService = helloWorldService;
+    }
+
+    OU o Autowired
+
+    */
 
     @GetMapping
     public String HelloWorld(){
-        return "Hello World!";
+        return helloWorldService.helloWorld("Jonathas");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") int id,@RequestParam(value = "filter", defaultValue = "nenhum") String filter, @RequestBody User body){
+        return ("Hello World, " + body.getName() + ". Idade: " + body.getIdade()  + " Filtro: " + filter);
     }
 }
