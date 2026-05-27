fb63465ace7524e2f1890107c95323e40f8a07b5
j0nathas <jonathasoliveira.2005@gmail.com>
2026-05-27 17:27:20 -0300

    Cors para acesso do front-end

--- backend/src/main/java/com/magna/gerenciamento_ferias/infra/SecurityConfig.java
+++ backend/src/main/java/com/magna/gerenciamento_ferias/infra/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.magna.gerenciamento_ferias.infra.security;
+package com.magna.gerenciamento_ferias.infra;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
--- backend/src/main/java/com/magna/gerenciamento_ferias/infra/TokenService.java
+++ backend/src/main/java/com/magna/gerenciamento_ferias/infra/TokenService.java
@@ -1,4 +1,4 @@
-package com.magna.gerenciamento_ferias.infra.security;
+package com.magna.gerenciamento_ferias.infra;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
