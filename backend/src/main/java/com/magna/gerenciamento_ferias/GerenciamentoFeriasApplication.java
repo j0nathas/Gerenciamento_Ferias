d8ffb86a297ecb2a59fcb9300ef6c36302c7bb2c
j0nathas <jonathasoliveira.2005@gmail.com>
2026-05-27 16:46:58 -0300

    Configuação de Spring Security e rotas de login e cadastro

---
+++ backend/src/main/java/com/magna/gerenciamento_ferias/GerenciamentoFeriasApplication.java
@@ -0,0 +1,13 @@
+package com.magna.gerenciamento_ferias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoFeriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoFeriasApplication.class, args);
+	}
+
+}
